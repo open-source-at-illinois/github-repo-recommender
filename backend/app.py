4e4258204a8afdb4f7c2a15a65bb9f691b76a7bd
Stephen Zhu <stephen.zhu.05@gmail.com>
2025-03-29 17:03:48 -0500

    Add response box + css works in flask

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from http.client import BAD_REQUEST, OK
+from flask import Flask, request, render_template
 
-app = Flask(__name__, template_folder='../frontend')
+app = Flask(__name__, template_folder='../frontend', static_folder='../frontend/static')
 
 @app.get('/')
 def home_page():
     return render_template('index.html')
+
+@app.post('/')
+def get_keywords():
+    keywords = request.form['keywords']
+
+    return {"message": f"keywords received: {keywords}"}, OK
+
 
 if __name__ == "__main__":
     app.run(host='localhost', debug=True)
