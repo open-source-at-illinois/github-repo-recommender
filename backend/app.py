7923b3973c3cc7dadc0985dd08e9827ba292081f
Stephen Zhu <stephen.zhu.05@gmail.com>
2025-03-29 17:41:59 -0500

    buttons send data + response box text gets updated (will change once linked with ML model)

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,18 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__, template_folder='../frontend', static_folder='../frontend/static')
 
+response = "Send some keywords!"
+
 @app.get('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', response=response)
 
 @app.post('/')
 def get_keywords():
     keywords = request.form['keywords']
+    response = f"keywords received: {keywords}"
 
+    return render_template('index.html', response=response)
     return {"message": f"keywords received: {keywords}"}, OK
 
 
